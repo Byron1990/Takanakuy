78d57cca163c2e59218cc33e3358973b645b405a
Byron <byron.mena.7@gmail.com>
2022-03-05 16:56:49 -0500

    Personaje puede saltar
    Personaje puede saltar y desplazarse

---
+++ Assets/Scripts/LogicFeet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicFeet : MonoBehaviour
+{
+    public Surface_Displacement surface_Displacement;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        //Si el personaje esta tocando una superficie puede saltar
+        surface_Displacement.puedeSaltar = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit other collider = " + other.name);
+        //Si el personaje sale de la superficie no puede saltar
+        surface_Displacement.puedeSaltar = false;
+    }
+}
--- Assets/Scripts/Surface_Displacement.cs
+++ Assets/Scripts/Surface_Displacement.cs
@@ -7,25 +7,53 @@ public class Surface_Displacement : MonoBehaviour
     public float velocidad_desplazamiento = 5f;
     public float velocidad_rotacion = 100f;
     private Animator animator;
+    public Rigidbody rb;
     /* Variables para reconocer si el personaje se mueve en planos X e Y */
     public float x, y;
+    /* Variable para controlar el salto */
+    public float fuerzaSalto;
+    public bool puedeSaltar;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        fuerzaSalto = 5f;
+        puedeSaltar = false;
     }
 
+    // FixedUpdate is called once per period time
+    void FixedUpdate()
+    {
+        /* Comando para realizar desplazamientos */
+        transform.Rotate(0, x * Time.deltaTime * velocidad_rotacion, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidad_desplazamiento);
+    }
     // Update is called once per frame
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-
-        /* Comando para realizar desplazamientos */
-        transform.Rotate(0, x*Time.deltaTime*velocidad_rotacion,0);
-        transform.Translate(0,0,y*Time.deltaTime*velocidad_desplazamiento);
-    
         animator.SetFloat("VelX", x);
         animator.SetFloat("VelY", y);
+        //Control de salto de la animacion
+        if (puedeSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, fuerzaSalto, 0), ForceMode.Impulse);
+                animator.SetBool("Salte", true);
+            }
+            animator.SetBool("TocarSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+    }
+    void EstoyCayendo()
+    {
+        animator.SetBool("TocarSuelo", false);
+        animator.SetBool("Salte", false);
     }
 }
